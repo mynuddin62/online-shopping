3e47ad9ad106e663b20f3e3a4f841eea5e4a5367
mynuddin62 <ratulmynuddin@yahoo.com>
2025-07-13 16:30:29 +0600

    [remove unused import]

--- src/test/java/com/faith/oneUmmah/service/ProductServiceImplTest.java
+++ src/test/java/com/faith/oneUmmah/service/ProductServiceImplTest.java
@@ -2,7 +2,6 @@ package com.faith.oneUmmah.service;
 
 import com.faith.oneUmmah.dto.ProductDTO;
 import com.faith.oneUmmah.repository.ProductRepository;
-import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
