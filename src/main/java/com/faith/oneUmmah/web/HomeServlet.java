27056ed38bf553d6b016313d283633a76dbdc35e
mynuddin62 <ratulmynuddin@yahoo.com>
2025-07-13 23:07:07 +0600

    [log add into HomeServlet]

--- src/main/java/com/faith/oneUmmah/web/HomeServlet.java
+++ src/main/java/com/faith/oneUmmah/web/HomeServlet.java
@@ -9,19 +9,24 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.List;
 
 @WebServlet("/home")
 public class HomeServlet extends HttpServlet {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HomeServlet.class);
 
     private ProductService productService = new ProductServiceImpl(new DummyProductRepositoryImpl());
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        LOGGER.info("Serving Home Page");
 
         List<ProductDTO> allProducts = productService.findAllProductSortedByName();
+        LOGGER.info("Total Product Found {}", allProducts.size());
 
         req.setAttribute("products", allProducts);
         req.getRequestDispatcher("WEB-INF/home.jsp").forward(req,res);
