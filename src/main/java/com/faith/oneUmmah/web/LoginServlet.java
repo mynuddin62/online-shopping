3c0d9fdd930f2b5c15d38d36de5f6c69f21388a7
mynuddin62 <ratulmynuddin@yahoo.com>
2025-07-18 18:28:51 +0600

    [user login api]

---
+++ src/main/java/com/faith/oneUmmah/web/LoginServlet.java
@@ -0,0 +1,69 @@
+package com.faith.oneUmmah.web;
+
+import com.faith.oneUmmah.domain.User;
+import com.faith.oneUmmah.dto.LoginDTO;
+import com.faith.oneUmmah.exception.UserNotFoundException;
+import com.faith.oneUmmah.repository.UserRepositoryImpl;
+import com.faith.oneUmmah.service.UserService;
+import com.faith.oneUmmah.service.UserServiceImpl;
+import com.faith.oneUmmah.util.ValidationUtil;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoginServlet.class);
+
+    private final UserService userService = new UserServiceImpl(new UserRepositoryImpl());
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        LOGGER.info("Serving login Page");
+        req.getRequestDispatcher("WEB-INF/login.jsp").forward(req,res);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        var loginDTO = new LoginDTO(req.getParameter("username"), req.getParameter("password"));
+        LOGGER.info("Received login data: {}", loginDTO);
+
+        var errors = ValidationUtil.getInstance().validate(loginDTO);
+
+        if(!errors.isEmpty()) {
+            LOGGER.info("Fail to login, sending to login form again");
+            req.setAttribute("errors", errors);
+            req.getRequestDispatcher("WEB-INF/login.jsp").forward(req,res);
+        }
+
+        login(loginDTO, req);
+        LOGGER.info("Login successful, redirecting to home page");
+        res.sendRedirect("/home");
+    }
+
+
+    private void login(LoginDTO loginDTO, HttpServletRequest req) throws UserNotFoundException {
+        //get the old session and invalidate
+        HttpSession oldSession = req.getSession(false);
+        if(oldSession != null){
+            oldSession.invalidate();
+        }
+
+        //now verify the user
+        User user = userService.verifyUser(loginDTO);
+
+        //put user in the session
+        HttpSession session = req.getSession(true);
+        session.setAttribute("user", user);
+
+    }
+
+
+}
---
+++ src/main/webapp/WEB-INF/login.jsp
@@ -0,0 +1,51 @@
+<%@include file="includes/header.jsp"  %>
+<%@include file="includes/navigation.jsp"  %>
+
+<div class="container mt-5 mb-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8 col-lg-6">
+            <div class="card shadow-sm">
+                <div class="card-body">
+                    <h3 class="card-title text-center mb-4">Login</h3>
+
+                    <!-- Login Form -->
+                    <form action="<c:url value='/login' />" method="post" class="row g-3">
+
+                        <!-- Username -->
+                        <div class="col-md-12">
+                            <label for="username" class="form-label">Username</label>
+                            <input class="form-control" id="username" name="username" required/>
+                            <c:if test="${errors.username != null}">
+                                <small class="text-danger">${errors.username}</small>
+                            </c:if>
+                        </div>
+
+                        <!-- Password -->
+                        <div class="col-md-12">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="password" name="password" required />
+                            <c:if test="${errors.password != null}">
+                                <small class="text-danger">${errors.password}</small>
+                            </c:if>
+                        </div>
+
+                        <!-- Submit Button -->
+                        <div class="col-12 d-grid mt-3">
+                            <button type="submit" class="btn btn-primary">Login</button>
+                        </div>
+
+                        <!-- Link to Signup -->
+                        <div class="col-12 text-center mt-2">
+                            <small>Don't have an account? <a class="btn-link" href="<c:url value='/signup' />">Sign up</a></small>
+                        </div>
+
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<%@include file="includes/footer.jsp"  %>
