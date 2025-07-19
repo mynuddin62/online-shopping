90291fab3bb7cf8519975b45a6d1ab616d208568
mynuddin62 <ratulmynuddin@yahoo.com>
2025-07-19 22:24:34 +0600

    [static admin user create with pass 1245]

--- src/main/java/com/faith/oneUmmah/repository/UserRepositoryImpl.java
+++ src/main/java/com/faith/oneUmmah/repository/UserRepositoryImpl.java
@@ -2,6 +2,10 @@ package com.faith.oneUmmah.repository;
 
 import com.faith.oneUmmah.domain.User;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
@@ -10,6 +14,21 @@ import java.util.concurrent.CopyOnWriteArraySet;
 public class UserRepositoryImpl implements UserRepository {
 
     private static final Set<User> USERS = new CopyOnWriteArraySet<>();
+
+    static {
+        try {
+            User admin = new User();
+            admin.setUsername("admin");
+            admin.setEmail("mynuddin.dev@gmail.com");
+            admin.setFirstName("Mynuddin");
+            admin.setLastName("Helal");
+            USERS.add(admin);
+            admin.setPassword(Base64.getEncoder().encodeToString(MessageDigest.getInstance("SHA-256").digest("1245".getBytes(StandardCharsets.UTF_8))));
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
     @Override
     public void save(User user) {
