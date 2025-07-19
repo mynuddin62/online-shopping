efa90ef3d310036662989e03b549da63655e93b0
mynuddin62 <ratulmynuddin@yahoo.com>
2025-07-19 20:14:28 +0600

    [ProductNotFoundException add]

---
+++ src/main/java/com/faith/oneUmmah/exception/ProductNotFoundException.java
@@ -0,0 +1,8 @@
+package com.faith.oneUmmah.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
