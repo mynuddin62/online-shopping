bbb833816c96a0252a1c8d35aeff67ad2db22034
mynuddin62 <ratulmynuddin@yahoo.com>
2025-07-20 11:58:23 +0600

    [remove productToCart add with its implementation, refactor code of cartServiceImpl ]

--- src/main/java/com/faith/oneUmmah/service/CartService.java
+++ src/main/java/com/faith/oneUmmah/service/CartService.java
@@ -6,4 +6,5 @@ import com.faith.oneUmmah.domain.User;
 public interface CartService {
     Cart getCartByUser(User currentUser);
     void addProductToCart(String productId, Cart cart);
+    void removeProductToCart(String productId, Cart cart);
 }
